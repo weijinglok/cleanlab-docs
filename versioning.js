a8876c73edef917a62b7c28eb1971f284842fb45
weijinglok <weijinglok@users.noreply.github.com>
2022-03-18 13:55:34 +0000

    deploy: weijinglok/cleanlab@4b877b8782e51af9d5a614834fcaaf359003ca56

--- versioning.js
+++ versioning.js
@@ -1,4 +1,4 @@
 var Version = {
-  version_number: "v2.0.1",
-  commit_hash: "faba74a9bccb3e76b9b7555e67c03ddbca27f948",
+  version_number: "v2.0.0",
+  commit_hash: "4b877b8782e51af9d5a614834fcaaf359003ca56",
 };
